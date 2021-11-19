b21060f1cd5f04377235677bf036b8d0fa00ef70
Dierickb <71480347+Dierickb@users.noreply.github.com>
2021-11-19 08:30:28 -0500

    j

--- controller/keys.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    database: {
-        host: 'database-taxiflow.c3snnsd75urd.us-west-2.rds.amazonaws.com', // HOST NAME
-        user: 'taxiflow', // USER NAME
-        database: 'taxiflow', // DATABASE NAME
-        password: 'taxiflow' // DATABASE PASSWORD
-    }
-}
