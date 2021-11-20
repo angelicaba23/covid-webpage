1f31d6bbbd3d90f1a946ed2700006c22dd9ad370
Dierickb <71480347+Dierickb@users.noreply.github.com>
2021-11-20 10:33:06 -0500

    Interfaz Login, falta el ROL

--- src/routes/links.js
+++ src/routes/links.js
@@ -7,8 +7,23 @@ router.get('/add',(req,res) => {
     res.send('Form');
 })
 
-router.post('/add',(req,res) => {
-    res.send('recived');
+router.post('/add', async (req,res) => {
+    const {
+        name, lastName, cedula, 
+        user, password
+    } = req.body;
+    const newLink = {
+        name, lastName, cedula, 
+        user, password
+    }
+    //links es el nombre de la tabla, ya despues modificas como va el codigo
+    //await pool.query('INSERT INTO links set ?', [newLink]);
+    res.send('Recived');
+})
+
+router.get('/', async(req,res) =>{
+    //const links = await pool.query('SELEC * FROM links');
+    res.send('List will be here');
 })
 
 module.exports = router;
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,25 +2,25 @@ const express = require('express');
 const router = express.Router();
 
 const getIndex = function (req, res, next) {
-    res.render('layouts/index', { title: 'TaxiFlow' });
+    res.render('layouts/index', { title: 'Stay Safe Home' });
 };
 const getBlog = function (req, res, next) {
-    res.render('layouts/blog', { title: 'TaxiFlow' });
+    res.render('layouts/blog', { title: 'Stay Safe Blog' });
 };
 const getContact = function (req, res, next) {
-    res.render('layouts/contact', { title: 'TaxiFlow' });
+    res.render('layouts/contact', { title: 'Stay Safe Contact' });
 };
 const getPrevention = function (req, res, next) {
-    res.render('layouts/prevention', { title: 'TaxiFlow' });
+    res.render('layouts/prevention', { title: 'Stay Safe Prevention' });
 };
 const getSymptoms = function (req, res, next) {
-    res.render('layouts/symptoms', { title: 'TaxiFlow' });
+    res.render('layouts/symptoms', { title: 'Stay Safe Symptoms' });
 };
 const getAbout = function (req, res, next) {
-    res.render('layouts/about', { title: 'TaxiFlow' });
+    res.render('layouts/about', { title: 'Stay Safe About' });
 };
 const getLogin = function (req, res, next) {
-    res.render('layouts/login', { title: 'TaxiFlow' });
+    res.render('layouts/login', { title: 'Stay Safe Login' });
 };
 
 module.exports = {
